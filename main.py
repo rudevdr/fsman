7265505a646d6fabbe1b93a20e215e66bc159b2e
rudevdr <gudayschief@gmail.com>
2019-01-30 01:30:40 +0530

    refactor for keeper
    1. move all indicators management task over to keeper module
    2. move inner functions to outside

--- main.py
+++ main.py
@@ -3,10 +3,22 @@ from curses.panel import *
 from glob import glob
 from indicator import *
 
+import provider
+import keeper
+
 from time import sleep
 
 #Source : https://docs.python.org/3.7/library/curses.html
 #Source2: https://docs.python.org/3.7/howto/curses.html
+
+def update_view(vieww):
+    vieww.erase()
+    vieww.box()
+    vieww.addstr(1, 3, ' '.join(keeper.get_all_obj()))
+
+def update():
+    update_panels()
+    doupdate()
 
 def main(stdscr):
     curs_set(0)
@@ -23,17 +35,7 @@ def main(stdscr):
 
     height, width = stdscr.getmaxyx()
 
-    def notify( text, posy=height-1, posx=0):
-        stdscr.clrtobot()
-        stdscr.addstr(posy, posx, text, color_pair(1))
-        update()
-
-    def update():
-        update_panels()
-        doupdate()
-
-
-    paths = glob('*')
+    paths = provider.glob()
     valid_lst_path = []
     lincs = []
     #Adds indicator
@@ -56,35 +58,7 @@ def main(stdscr):
         lst_window.addstr(lst_y, lst_x, path, color_pair(2)| A_REVERSE)
         valid_lst_path.append((path, line_y + lst_y, line_x + lst_x))
 
-    dinc = Indicator(valid_lst_path, dinc_text, 0, color_pair(2) | A_REVERSE)
-
-    def all_objects(lincs):
-        lincs_obj = [inc.current_obj for inc in lincs]
-        dinc_obj = dinc.current_obj
-        if dinc_obj in lincs_obj:
-            return lincs_obj
-        else:
-            return lincs_obj + [dinc_obj]
-
-    def toggle_inc(dinc):
-        index = dinc.current_index
-        if index in [inc.current_index for inc in lincs]:
-            inc = [inc for inc in lincs if inc.current_index == index][0]
-            #inc = lincs[index]
-            inc.delete_inc()
-            lincs.remove(inc)
-        else:
-            new_inc = Indicator(valid_lst_path, dinc_text, index, color_pair(1) | A_REVERSE)
-            #new_inc.move_to_bottom()
-            lincs.append(new_inc)
-
-        dinc.move_to_top()
-
-    def update_view(vieww):
-        vieww.erase()
-        vieww.box()
-        vieww.addstr(1, 3, ' '.join(all_objects(lincs)))
-
+    keeper.add_didc(Indicator(valid_lst_path, dinc_text, 0, color_pair(2) | A_REVERSE))
 
     #.Add viewo
     viewo_padding = 50
@@ -95,11 +69,10 @@ def main(stdscr):
     viewo_window = newwin(viewo_height, viewo_width, viewo_y, viewo_x)
     viewo_panel = new_panel(viewo_window)
     viewo_window.box()
-    viewo_window.addstr(1, 1, str(all_objects(lincs)))
+    viewo_window.addstr(1, 1, str(keeper.get_all_obj()))
 
     stdscr.refresh()
     update()
-
 
     while True:
         key = stdscr.getch()
@@ -107,20 +80,19 @@ def main(stdscr):
             stdscr.addstr(height-1, 0, "Press any key to exit", color_pair(1))
             break
         elif key == ord('j'):
-            dinc.move_down()
+            keeper.move_didc_down()
+            #dinc.move_down()
         elif key == ord('k'):
-            dinc.move_up()
+            keeper.move_didc_up()
+            #dinc.move_up()
         elif key == ord('n'):
             stdscr.addstr(height-1, 0, inc.current_obj, color_pair(1))
             stdscr.clrtoeol()
         elif key == ord('t'):
-            toggle_inc(dinc)
+            keeper.toggle_idc(Indicator(valid_lst_path, dinc_text, keeper.get_didc_index(), color_pair(1) | A_REVERSE))
 
         update_view(viewo_window)
         update()
-        #notify(str(dinc.current_index+1)+'/' +str(len(lincs))+'/'+str([inc.current_index+1 for inc in lincs]))
-        #notify(str(all_objects(lincs)))
-
 
 if __name__ == '__main__':
     wrapper(main)
