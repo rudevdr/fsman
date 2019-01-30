c58a836ec6fbbc6ede61b84d45356da719289bee
rudevdr <gudayschief@gmail.com>
2019-01-31 01:07:33 +0530

    move from main.py to show output window

---
+++ view_output.py
@@ -0,0 +1,20 @@
+from curses import newwin, doupdate
+from curses.panel import new_panel, update_panels
+
+def update_view(vieww, keeper):
+    vieww.erase()
+    vieww.box()
+    vieww.addstr(1, 3, ' '.join(keeper.get_all_obj()))
+
+def init(viewo_padding, line_y, lst_window_width, keeper):
+
+    viewo_y = line_y
+    viewo_x = lst_window_width + viewo_padding
+    viewo_height = 20
+    viewo_width = 30
+    viewo_window = newwin(viewo_height, viewo_width, viewo_y, viewo_x)
+    #viewo_window.bkgd(color_pair(1))
+    viewo_panel = new_panel(viewo_window)
+    viewo_window.box()
+    viewo_window.addstr(1, 1, str(keeper.get_all_obj()))
+    return viewo_window
