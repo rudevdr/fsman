c666d27cd52da400d313d7f0c9c0576ac944407e
rudevdr <gudayschief@gmail.com>
2019-02-15 02:10:58 +0530

    read only python files

--- provider.py
+++ provider.py
@@ -1,7 +1,7 @@
 from glob import glob
 
 #todo: fetch pattern from cfg
-program_paths = glob('tests/*')
+program_paths = glob('tests/*py')
 #program_paths = glob('keeper/test_scripts/*')
 
 def glob():
