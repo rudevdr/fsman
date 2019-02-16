1eecb7425fc9cdeb40fa05580938395b1c6dbf3e
rudevdr <gudayschief@gmail.com>
2019-02-16 18:50:49 +0530

    intial commit, replaces config_grabber.py

---
+++ configurer.py
@@ -0,0 +1,55 @@
+import configparser
+
+CONFIG = "fsman.cfg"
+
+default_config = {
+    'Provider': {
+        'source_file': 'glob'
+    }
+}
+
+
+def configread():
+    config = configparser.ConfigParser()
+    config.read(CONFIG)
+
+    return config
+
+
+def all_keys():
+    config = configread()
+
+    for section in config.sections():
+        for key in config.options(str(section)):
+            yield key
+
+
+def get(item):
+    config = configread()
+
+    for section in config.sections():
+        for key in config.options(str(section)):
+            if item == key:
+                return config.get(section, key).strip("'").strip('"')
+
+    if item in add_keys_default():
+        #TODO: if key not found in config, then check if key is in default dictionary. if it is then add to file and return the default value, else raise error
+        add_default(key, value)
+        return default
+    else:
+        raise KeyError(f'Setting for "{item}" is not found in {CONFIG}')
+
+
+def all_keys_default():
+    '''
+    returns a tuple of key, value of all dict inside dict (secondary dicts)
+    '''
+    #TODO: see doc
+    yield default_config.keys()
+
+def add_default(key, value):
+    '''
+    writes key, value to CONFIG file
+    '''
+
+    #TODO: see doc
